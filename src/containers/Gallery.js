84fc26a8c3751688fdbbe6d5f6508ff1284d1050
Kuba <kubaz9991@gmail.com>
2019-08-19 23:18:01 +0200

    Render images on componentDidMount

--- src/containers/Gallery.js
+++ src/containers/Gallery.js
@@ -1,9 +1,31 @@
 import React from "react";
+import { fetchPictures } from "../actions/actions";
+import { connect } from "react-redux";
 
-export default function Gallery() {
-  return (
-    <div>
-      <p>Gallery stuff</p>
-    </div>
-  );
+class Gallery extends React.Component {
+  componentDidMount() {
+    this.props.dispatch(fetchPictures());
+  }
+  render() {
+    return (
+      <div>
+        {this.props.data.map(item => {
+          return (
+            <ul>
+              <li>
+                <img src={item.media.m} alt='marilyn' />
+              </li>
+              ;
+            </ul>
+          );
+        })}
+      </div>
+    );
+  }
 }
+
+const mapStateToProps = state => ({
+  data: state.data
+});
+
+export default connect(mapStateToProps)(Gallery);
