a7119a56c065d36beb2bde0aadb91fbe6d8f8a8c
Kuba <kubaz9991@gmail.com>
2019-08-20 20:28:59 +0200

    fix redux devtools

--- src/index.js
+++ src/index.js
@@ -4,16 +4,14 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import marilynReducer from "./reducers/marilynReducer";
 import { BrowserRouter } from "react-router-dom";
-import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
 
-const store = createStore(
-  marilynReducer,
-  composeWithDevTools(applyMiddleware(thunk))
-);
+let devTools = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(marilynReducer, devTools(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
