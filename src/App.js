6aff17df726e2b306bddc82ecde8832a30ad1ed5
Kuba <kubaz9991@gmail.com>
2019-08-20 22:26:59 +0200

    Style header - styled components + fontawesome

--- src/App.js
+++ src/App.js
@@ -3,9 +3,16 @@ import Layout from "./hoc/Layout";
 import Gallery from "./containers/Gallery";
 import Profile from "./containers/Profile";
 import { Switch, Route, Redirect } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
+const GlobalStyles = createGlobalStyle`
+  * {
+  box-sizing: border-box;
+  }
+`;
 function App() {
   return (
     <div className='App'>
+      <GlobalStyles />
       <Layout>
         <Switch>
           <Route exact path='/' render={() => <Redirect to='/gallery' />} />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,78 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+import sand_rock from "../assets/sand_rock.jpg";
+import marilyn_avatar from "../assets/marilyn_avatar.png";
+const StyledHeader = styled.header`
+  width: 100%;
+  height: 27vh;
+`;
+
+const NavBackground = styled.div`
+  background-image: url(${sand_rock});
+  background-position: 75% 70%;
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 80%;
+  width: 100%;
+`;
+
+const NavBar = styled.nav`
+  width: 100%;
+  height: 20%;
+  background-color: #79d6bb;
+  border-top: 1px solid white;
+  position: relative;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  padding-right: 50px;
+`;
+
+const Avatar = styled.img`
+  width: 100px;
+  height: 100px;
+  z-index: 2;
+  position: absolute;
+  top: -40px;
+  left: 40px;
+  border-radius: 100px;
+  border: 4px solid white;
+`;
+
 export default function Navbar() {
   return (
-    <div>
-      <p>Hello from navbar</p>
-      <Link to='/gallery'>Gallery</Link>
-      <Link to='/profile'>Profile</Link>
-    </div>
+    <StyledHeader>
+      <NavBackground />
+      <NavBar>
+        <Avatar src={marilyn_avatar} />
+
+        <Link to='/profile'>
+          <i
+            class='fas fa-user'
+            style={{
+              color: "white",
+              border: "1px solid white",
+              borderRadius: "50%",
+              padding: "10px",
+              marginRight: "10px",
+              height: "90%"
+            }}
+          />
+        </Link>
+        <Link to='/gallery'>
+          <i
+            class='far fa-image'
+            style={{
+              color: "white",
+              border: "1px solid white",
+              borderRadius: "50%",
+              padding: "10px",
+              height: "90%"
+            }}
+          />
+        </Link>
+      </NavBar>
+    </StyledHeader>
   );
 }
