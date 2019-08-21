d5a0048ae97603d0be04535d4d09dac2450f8c47
Kuba <kubaz9991@gmail.com>
2019-08-21 22:02:39 +0200

    Profile page styling

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -1,13 +1,48 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledContainer = styled.div`
+  width: 80%;
+  margin: 30px;
+`;
+const StyledH1 = styled.h1`
+  letter-spacing: -2px;
+  margin-bottom: 5px;
+`;
+const StyledText = styled.p`
+  width: 100%;
+  margin: 25px 0;
+`;
+
+const StyledLocation = styled.p`
+  color: grey;
+  margin: 0;
+`;
+
+const StyledBlockquote = styled.blockquote`
+  width: 100%;
+  font-style: italic;
+  border-left: 4px solid #79d6bb;
+  color: grey;
+  font-family: "Times New Roman", Times, serif;
+  font-size: 1.2rem;
+  width: 70%;
+  padding-left: 15px;
+`;
 
 export default function Profile() {
   return (
-    <div>
-      <h1>Marilyn Monroe</h1>
-      <p>icon Poznan, PL</p>
+    <StyledContainer>
+      <StyledH1>Marilyn Monroe</StyledH1>
+      <StyledLocation>
+        <span style={{ marginRight: "5px" }}>
+          <i className='fas fa-map-marker-alt' />
+        </span>
+        Poznan, PL
+      </StyledLocation>
 
       <article>
-        <p>
+        <StyledText>
           Marilyn Monroe (born Norma Jeane Mortenson; June 1, 1926 – August 4,
           1962) was an American actress, model, and singer. Famous for playing
           comic "blonde bombshell" characters, she became one of the most
@@ -17,19 +52,17 @@ export default function Profile() {
           (equivalent to $2 billion in 2018) by the time of her unexpected death
           in 1962. More than half a century later, she continues to be a major
           popular culture icon.
-        </p>
-        <blockquote cite='https://en.wikiquote.org/wiki/Marilyn_Monroe'>
-          <i>
-            The truth is I've never fooled anyone. I've let men sometimes fool
-            themselves. Men sometimes didn’t bother to find out who and what I
-            was. Instead they would invent a character for me. I wouldn't argue
-            with them. They were obviously loving somebody I wasn't. When they
-            found this out, they would blame me for disillusioning them — and
-            fooling them.
-          </i>
-        </blockquote>
+        </StyledText>
+        <StyledBlockquote cite='https://en.wikiquote.org/wiki/Marilyn_Monroe'>
+          The truth is I've never fooled anyone. I've let men sometimes fool
+          themselves. Men sometimes didn’t bother to find out who and what I
+          was. Instead they would invent a character for me. I wouldn't argue
+          with them. They were obviously loving somebody I wasn't. When they
+          found this out, they would blame me for disillusioning them — and
+          fooling them.
+        </StyledBlockquote>
 
-        <p>
+        <StyledText>
           Monroe's troubled private life received much attention. She struggled
           with substance abuse, depression, and anxiety. Her second and third
           marriages, to retired baseball star Joe DiMaggio and playwright Arthur
@@ -38,8 +71,8 @@ export default function Profile() {
           in Los Angeles. Although Monroe's death was ruled a probable suicide,
           several conspiracy theories have been proposed in the decades
           following her death.
-        </p>
+        </StyledText>
       </article>
-    </div>
+    </StyledContainer>
   );
 }
