c32c9e9320a2af344ec34f224d62239730ba6094
Kuba <kubaz9991@gmail.com>
2019-08-21 20:59:31 +0200

    Import loader spinner stylesheet

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import marilynReducer from "./reducers/marilynReducer";
 import { BrowserRouter } from "react-router-dom";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
 let devTools = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
